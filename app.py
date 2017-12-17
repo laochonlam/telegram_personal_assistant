a72b02239f27e1d79cb32a2de8f902a7504c333a
laochanlam <laochanlam@gmail.com>
2017-12-17 14:53:57 +0800

    chat bot init

---
+++ app.py
@@ -0,0 +1,75 @@
+import sys
+from io import BytesIO
+
+import telegram
+from flask import Flask, request, send_file
+
+from fsm import TocMachine
+
+
+API_TOKEN = '474099509:AAGgL9LPw-rwHPLr57JAlHxtRqFKc7N52Sw'
+WEBHOOK_URL = '127.0.0.1/hook'
+
+app = Flask(__name__)
+bot = telegram.Bot(token=API_TOKEN)
+machine = TocMachine(
+    states=[
+        'user',
+        'state1',
+        'state2'
+    ],
+    transitions=[
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'state1',
+            'conditions': 'is_going_to_state1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'state2',
+            'conditions': 'is_going_to_state2'
+        },
+        {
+            'trigger': 'go_back',
+            'source': [
+                'state1',
+                'state2'
+            ],
+            'dest': 'user'
+        }
+    ],
+    initial='user',
+    auto_transitions=False,
+    show_conditions=True,
+)
+
+
+def _set_webhook():
+    status = bot.set_webhook(WEBHOOK_URL)
+    if not status:
+        print('Webhook setup failed')
+        sys.exit(1)
+    else:
+        print('Your webhook URL has been set to "{}"'.format(WEBHOOK_URL))
+
+
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    machine.advance(update)
+    return 'ok'
+
+
+@app.route('/show-fsm', methods=['GET'])
+def show_fsm():
+    byte_io = BytesIO()
+    machine.graph.draw(byte_io, prog='dot', format='png')
+    byte_io.seek(0)
+    return send_file(byte_io, attachment_filename='fsm.png', mimetype='image/png')
+
+
+if __name__ == "__main__":
+    _set_webhook()
+    app.run()
