95123476f6e19db782b6e32b4bbdceb7fa56e993
laochanlam <laochanlam@gmail.com>
2017-12-30 19:32:29 +0800

    translation & film query function completed

--- app.py
+++ app.py
@@ -2,42 +2,76 @@ import sys
 from io import BytesIO
 
 import telegram
+# telegram api
+# https://python-telegram-bot.readthedocs.io/en/stable/
+
 from flask import Flask, request, send_file
 
 from fsm import TocMachine
+# fsm api
+# https://github.com/pytransitions/transitions#transitions
 
 
-API_TOKEN = '474099509:AAGgL9LPw-rwHPLr57JAlHxtRqFKc7N52Sw'
-WEBHOOK_URL = 'https://bot.laochanlam.me'
+API_TOKEN = '481103945:AAGF-yzyQNX7f-XxPo_yG500j5UWw6ZYLsY'
+WEBHOOK_URL = 'https://657a2c7f.ngrok.io/hook'
 
 app = Flask(__name__)
 bot = telegram.Bot(token=API_TOKEN)
+
 machine = TocMachine(
     states=[
         'user',
-        'state1',
-        'state2'
+        'translation_mode',
+        'translating',
+        'film_query_mode',
+        'film_querying'
     ],
     transitions=[
+        # translate function here
         {
-            'trigger': 'advance',
+            'trigger': 'choose_mode',
             'source': 'user',
-            'dest': 'state1',
-            'conditions': 'is_going_to_state1'
+            'dest': 'translation_mode',
+            'conditions': 'is_going_to_translation_mode'
         },
         {
-            'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state2',
-            'conditions': 'is_going_to_state2'
+            'trigger': 'translate',
+            'source': 'translation_mode',
+            'dest': 'user',
+            'conditions': 'is_going_back_to_user'
         },
         {
-            'trigger': 'go_back',
-            'source': [
-                'state1',
-                'state2'
-            ],
-            'dest': 'user'
+            'trigger': 'translate',
+            'source': 'translation_mode',
+            'dest': 'translating',
+        },
+        {
+            'trigger': 'go_back_to_translation_mode',
+            'source': 'translating',
+            'dest': 'translation_mode'
+        },
+        # film query function here
+        {
+            'trigger': 'choose_mode',
+            'source': 'user',
+            'dest': 'film_query_mode',
+            'conditions': 'is_going_to_film_query_mode'
+        },
+        {
+            'trigger': 'film_query',
+            'source': 'film_query_mode',
+            'dest': 'user',
+            'conditions': 'is_going_back_to_user'
+        },
+        {
+            'trigger': 'film_query',
+            'source': 'film_query_mode',
+            'dest': 'film_querying',
+        },
+        {
+            'trigger': 'go_back_to_film_query_mode',
+            'source': 'film_querying',
+            'dest': 'film_query_mode'
         }
     ],
     initial='user',
@@ -55,10 +89,16 @@ def _set_webhook():
         print('Your webhook URL has been set to "{}"'.format(WEBHOOK_URL))
 
 
-@app.route('/', methods=['POST'])
+@app.route('/hook', methods=['POST'])
 def webhook_handler():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
-    machine.advance(update)
+    if (machine.is_user()):
+        machine.choose_mode(update, bot)
+    elif (machine.is_translation_mode()):
+        machine.translate(update, bot)
+    elif (machine.is_film_query_mode()):
+        machine.film_query(update, bot)
+        
     return 'ok'
 
 
--- fsm.py
+++ fsm.py
@@ -1,31 +1,96 @@
-from transitions.extensions import GraphMachine
+# -*- coding: utf-8 -*-
 
+from transitions.extensions import GraphMachine
+from googletrans import Translator
+
+from lxml import etree, html
+import requests
+import json
+import urllib.request
+
+translator = Translator()
 
 class TocMachine(GraphMachine):
+    
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(
             model = self,
             **machine_configs
         )
 
-    def is_going_to_state1(self, update):
+    # Translate function Here
+    def is_going_to_translation_mode(self, update, bot):
         text = update.message.text
-        return text.lower() == 'go to state1'
+        print("translate detect")
+        return text.lower() == '/translate' or text.lower() == '/t'
 
-    def is_going_to_state2(self, update):
+    def on_enter_translation_mode(self, update, bot):
+        update.message.reply_text("translation mode entered.")
+        print('Entering translate mode')
+        
+    def on_exit_translation_mode(self, update, bot):
+        update.message.reply_text("translation mode exited.")
+        print('Leaving translate mode')
+
+    def on_enter_translating(self, update, bot):
+        translate_detect_object = translator.detect(update.message.text)
+        if (translate_detect_object.lang.find("zh-CN") != -1) or (translate_detect_object.lang.find("zh-tw") != -1):
+            print("find chinese")
+            translated_object = translator.translate(
+                update.message.text, src="zh-tw", dest="en")
+        else:
+            print("no find chinese")
+            translated_object = translator.translate(
+                update.message.text, dest="zh-tw")
+        # print(translate_detect_object.lang)
+        print(translated_object)
+        update.message.reply_text(translated_object.text)
+        self.go_back_to_translation_mode(update, bot)
+
+    def on_exit_translating(self, update, bot):
+        print('Leaving translating')
+
+    # film query function Here
+    def is_going_to_film_query_mode(self, update, bot):
         text = update.message.text
-        return text.lower() == 'go to state2'
+        print("film query detect")
+        return text.lower() == 'filmquery' or text.lower() == '/f'
 
-    def on_enter_state1(self, update):
-        update.message.reply_text("I'm entering state1")
-        self.go_back(update)
+    def on_enter_film_query_mode(self, update, bot):
+        update.message.reply_text("film query mode entered.")
+        print('Entering film query mode')
 
-    def on_exit_state1(self, update):
-        print('Leaving state1')
+    def on_exit_film_query_mode(self, update, bot):
+        update.message.reply_text("film query mode exited.")
+        print('Leaving film query mode')
 
-    def on_enter_state2(self, update):
-        update.message.reply_text("I'm entering state2")
-        self.go_back(update)
+    def on_enter_film_querying(self, update, bot):
+        print("Enter film querying")
+        result = requests.get("http://www.atmovies.com.tw/showtime/t06607/a06/")
+        result.encoding = "UTF8"
+        root = etree.fromstring(result.content, etree.HTMLParser())
+        entry_filmname = update.message.text
+        # Get film Title
+        for film in root.xpath("//ul[@id=\"theaterShowtimeTable\"]"):
+            filmname = film.xpath("./li[@class=\"filmTitle\"]/a/text()")
+            if (filmname[0].find(entry_filmname) != -1):
+                filmTimetable = film.xpath("./li[position()=2]/ul[position()=2]/li/text()")
+                imageurl = film.xpath("./li[position()=2]/ul[position()=1]/li/a/img/@src")
+                print(imageurl[0])
+                tmp = "電影名稱: %s" % (filmname[0])
+                # reply 
+                bot.send_photo(chat_id=update.message.chat_id,
+                               photo=imageurl[0])
+                update.message.reply_text(tmp)
+                for time in filmTimetable:
+                    update.message.reply_text(time)
 
-    def on_exit_state2(self, update):
-        print('Leaving state2')
+        self.go_back_to_film_query_mode(update, bot)
+
+    def on_exit_film_querying(self, update, bot):
+        print('Leaving film querying')
+
+    # General
+    def is_going_back_to_user(self, update, bot):
+        text = update.message.text
+        return text.lower() == '/back' or text.lower() == '/b'
